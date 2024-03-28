d4ec56d8265675a9e8e5f605f12754e778d7d5b3
Siddartha Koppaka <siddarthakoppaka@gmail.com>
2024-03-28 12:20:39 -0400

    New

--- src/main/java/com/shacheru/primesservice/Controller/primeController.java
+++
@@ -1,4 +0,0 @@
-package com.shacheru.primesservice.Controller;
-
-public class primeController {
-}
---
+++ src/main/java/com/siddartha/primesservice/services/IPrimeService.java
@@ -0,0 +1,5 @@
+package com.amit.primesservices.services;
+
+public interface IPrimeService {
+    boolean isPrime(long n);
+}
